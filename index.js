6c12df756733f17714ddf43555592487665e1b16
Montesit00 <marcos.montellanok@gmail.com>
2022-09-14 12:38:03 -0300

    Primer Server

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 //Conexiones
+const path = require('path')
 const express = require('express')
 const dato = express();
 
 //Uso los datos de los otros archivos
 dato.use(express.json())
 dato.use(require('./src/rutas/rutas'))
+
+//recurso estatico
+dato.use(express.static(path.join(__dirname, "public")))
 
 //Compruebo si funciona el servidor
 dato.listen(3000,()=>{
