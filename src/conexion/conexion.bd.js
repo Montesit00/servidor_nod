73abfaffd2dc43f9915a662aa39b1e5a93e86b55
Montesit00 <marcos.montellanok@gmail.com>
2022-09-23 09:57:33 -0300

    actualizacion 23-9-22

--- src/conexion/conexion.bd.js
+++ src/conexion/conexion.bd.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://monte:monte123@monte.xnfkexl.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://monte:monte123@monte.xnfkexl.mongodb.net/progra?retryWrites=true&w=majority')
 .then(()=>console.log('Conectado'))
 .catch((err)=>console.log(err))
